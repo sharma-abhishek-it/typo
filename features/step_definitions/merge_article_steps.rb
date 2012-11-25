7ac9b81dff4be6a525aac0778f45b67659a0cbee
Abhishek Sharma <sharma dot abhishek dot it at-sign gmail dot com>
2012-11-25 09:25:43 +0530

    Step definitions for merging articles feature

---
+++ features/step_definitions/merge_article_steps.rb
@@ -0,0 +1,7 @@
+Given /^the following articles exist$/ do |articles|
+    # table is a Cucumber::Ast::Table
+    articles.hashes.each do |article|
+        puts "nothing"
+    end
+    pending # express the regexp above with the code you wish you had
+end
